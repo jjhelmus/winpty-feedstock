1a06306b93e935edd1958fcda76ae63fe9e494b0
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2017-06-17 14:31:20 -0500

    Generate nmake files by default

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -20,7 +20,7 @@ cd src
 curl https://raw.githubusercontent.com/conda-forge/winpty-feedstock/master/recipe/CMakeLists.txt -o CMakeLists.txt
 dir
 
-%LIBRARY_BIN%\cmake -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" -DCMAKE_BUILD_TYPE:STRING=Release
+%LIBRARY_BIN%\cmake -G "NMake Makefiles" -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" -DCMAKE_BUILD_TYPE:STRING=Release
 if errorlevel 1 (
     echo error: cmake failed
     exit /b 1
